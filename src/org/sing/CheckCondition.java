1a37e0d31630e6f1cafe04c716f99ce3074b6d70
RajaSharmila <sharmila851998@gmail.com>
2023-02-04 11:49:03 +0530

    varun details

--- src/org/sing/CheckCondition.java
+++ src/org/sing/CheckCondition.java
@@ -5,5 +5,7 @@ public class CheckCondition {
 	public static void main(String[] args) {
 		String s = "Biscuit";
 		System.out.println(s);
+		boolean empty = s.isEmpty();
+		System.out.println(empty);
 	}
 }
